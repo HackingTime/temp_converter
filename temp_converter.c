d5b7be1ae22e460997eb956858384f6accd21c14
HackingTime <gcgithub@bar.edu>
2016-12-01 18:22:00 -0800

    fully functional. bug fixes.
    fixed the core dumping problem

--- temp_converter.c
+++ temp_converter.c
@@ -1,17 +1,33 @@
 #import <stdio.h>
+#include <stdlib.h>
+
 
 #define FRZPT 32.0f
 #define SCLFACT (5.0f/9.0f)
 
-int main(int argc, char *argv[])
+
+int
+main(int argc, char *argv[])
 {
 float celc;
-float fahr;
-fahr = atoi{argv[1]};
-pritnf("argv = %d",fahr);
-celc = (fahr - FRZPT) * SCLFACT;
-printf("Celcius:%.1f\n",celc);
+int fahr;
+if (argc < 2){
+//if no value is specified
+        printf("Usage: %s [fahrenheit]\n",argv[0]);
+        exit(1);
+
+        }
+
+fahr = atoi(argv[1]);
+
+printf ("fahrenheit = %d\n",fahr);
+//make calc
+celc = ((fahr - FRZPT) * SCLFACT);
+//print output
+printf ("Celcius:%.1f\n",celc);
 
 
 
 }
+
+
